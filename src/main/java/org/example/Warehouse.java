cb08eaf32ddf8c94510f2f3d05a32cdc78a71843
TheSecondChance <roenko.dv@gmail.com>
2024-07-29 18:13:11 +0300

    Added solution

--- src/main/java/org/example/Warehouse.java
+++ src/main/java/org/example/Warehouse.java
@@ -3,18 +3,26 @@ package org.example;
 import lombok.Getter;
 import lombok.extern.log4j.Log4j2;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 @Log4j2
 @Getter
 public class Warehouse extends Thread {
 
+    private final Lock truckLock, loadUnloadLock;
+    private final Condition arrivingTruck, loadUnloadTruck;
     private final List<Block> storage = new ArrayList<>();
+    private final Queue<Truck> arrivedTrucks = new LinkedList<>(); // Очередь прибывших грузовиков
 
     public Warehouse(String name) {
         super(name);
+        truckLock = new ReentrantLock();
+        loadUnloadLock = new ReentrantLock();
+        arrivingTruck = truckLock.newCondition();
+        loadUnloadTruck = loadUnloadLock.newCondition();
     }
 
     public Warehouse(String name, Collection<Block> initialStorage) {
@@ -32,7 +40,6 @@ public class Warehouse extends Thread {
                     sleep(100);
                 } catch (InterruptedException e) {
                     if (currentThread().isInterrupted()) {
-
                         break;
                     }
                 }
@@ -56,22 +63,42 @@ public class Warehouse extends Thread {
         } catch (InterruptedException e) {
             log.error("Interrupted while loading truck", e);
         }
-        truck.getBlocks().addAll(blocksToLoad);
+
+        loadUnloadLock.lock();
+        try {
+            truck.getBlocks().addAll(blocksToLoad);
+            loadUnloadTruck.signal(); // Сигнализируем методу arrive() о полной загрузке грузовика
+        } finally {
+            loadUnloadLock.unlock();
+        }
+
         log.info("Truck loaded {}", truck.getName());
     }
 
     private Collection<Block> getFreeBlocks(int maxItems) {
-        //TODO необходимо реализовать потокобезопасную логику по получению свободных блоков
-        //TODO 1 блок грузится в 1 грузовик, нельзя клонировать блоки во время загрузки
-        List<Block> blocks = new ArrayList<>();
-        for (int i = 0; i < maxItems; i++) {
-            blocks.add(new Block());
+        // Захватываем блокировку для безопасного извлечения блоков из хранилища
+        loadUnloadLock.lock();
+        try {
+            List<Block> blocks = new ArrayList<>();
+            for (int i = 0; i < maxItems; i++) {
+                blocks.add(storage.remove(0));
+            }
+            return blocks;
         }
-        return blocks;
+        finally {
+            loadUnloadLock.unlock();
+        }
     }
 
     private void returnBlocksToStorage(List<Block> returnedBlocks) {
-        //TODO реализовать потокобезопасную логику по возврату блоков на склад
+        // Захватываем блокировку для безопасного добавления блоков в хранилище
+        loadUnloadLock.lock();
+        try {
+            storage.addAll(returnedBlocks);
+            loadUnloadTruck.signal(); // Сигнализируем методу arrive() о полной разгрузке грузовика
+        } finally {
+            loadUnloadLock.unlock();
+        }
     }
 
     private void unloadTruck(Truck truck) {
@@ -88,13 +115,43 @@ public class Warehouse extends Thread {
     }
 
     private Truck getNextArrivedTruck() {
-        //TODO необходимо реализовать логику по получению следующего прибывшего грузовика внутри потока склада
-        return null;
+        truckLock.lock();
+        try {
+            // Ожидаем прибытия грузовика
+            while (arrivedTrucks.isEmpty()) {
+                arrivingTruck.await();
+            }
+        } catch (InterruptedException e) {
+            log.info("Warehouse thread interrupted");
+        } finally {
+            truckLock.unlock();
+        }
+        // Берём грузовик из очереди прибывших
+        return arrivedTrucks.poll();
     }
 
 
     public void arrive(Truck truck) {
-        //TODO необходимо реализовать логику по сообщению потоку склада о том, что грузовик приехал
-        //TODO так же дождаться разгрузки блоков, при возврате из этого метода - грузовик покинет склад
+        // По прибытии грузовика, добавляем его в список ожидающих у склада
+        // и сигнализируем методу getNextArrivedTruck() о прибытии грузовика
+        truckLock.lock();
+        try {
+            arrivedTrucks.add(truck);
+            arrivingTruck.signal();
+        } finally {
+            truckLock.unlock();
+        }
+
+        // Ожидаем загрузки/разгрузки грузовика
+        loadUnloadLock.lock();
+        try {
+            loadUnloadTruck.await();
+        }
+        catch (InterruptedException e) {
+            log.info("Warehouse thread interrupted");
+        }
+        finally {
+            loadUnloadLock.unlock();
+        }
     }
 }
